8b2ca4f08aeba54c95b66d821525cd987323a249
dmalzl <UaF20091!>
2024-03-14 13:36:30 +0100

    add KeyError monitor for GSE parsing

--- metadatamapping/geo.py
+++ metadatamapping/geo.py
@@ -85,9 +85,15 @@ def fetch_and_parse_gse_softs(
             continue
         
         gse_metadata = parsers.parse_soft_metadata(gse_soft, gse_keys)
-        this_submission_date = parsers.to_date(
-            gse_metadata['series_submission_date']
-        )
+        try:
+            this_submission_date = parsers.to_date(
+                gse_metadata['series_submission_date']
+            )
+        
+        except KeyError as e:
+            logging.error(f'could not retrieve submission date for {gse}')
+            raise e
+        
         gse_metadata['series_submission_date'] = this_submission_date
         
         if not original_series_metadata:
